1f20b41bbd279a1e57ff335beafe0653226a91ed
DrewArg <andresfabbiano5@gmail.com>
2022-05-19 02:00:57 -0300

    handlebars 2

--- Desafios/D5/src/router/web/webRouter.js
+++ Desafios/D5/src/router/web/webRouter.js
@@ -15,7 +15,38 @@ webRouter.get('/datos', async (req, res) => {
         nombres,
         hayNombres: Boolean(nombres.length > 0),
     }
-    res.render('datos', datos)
+    res.render('./partials/datos', datos)
 })
+
+
+webRouter.get('/servicio', (req, res) => {
+    res.render('./partials/servicio', {
+        titulo: 'Servicio',
+        estado: true,
+        servicio: 'Curso de JavaScript'
+    })
+})
+
+
+webRouter.get('/equipo', (req, res) => {
+    res.render('./partials/equipo', {
+        titulo: 'Equipo',
+        // equipo: ['Diego', 'Only Players']
+        equipo: [
+            { id: 1, nombre: 'Javier' },
+            { id: 2, nombre: 'Steve' },
+            { id: 3, nombre: 'Ignacio' },
+        ]
+    })
+})
+
+
+webRouter.get('/*', (req, res, next) => {
+    res.status(404).render('./partials/404', {
+        titulo: '404',
+        descripcion: "Página no enconrtada"
+    })
+})
+
 
 module.exports = { webRouter }

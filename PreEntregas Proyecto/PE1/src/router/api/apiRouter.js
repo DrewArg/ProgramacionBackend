c8989c0b818fb4ead51005a23bfb5edb5e6ff19e
DrewArg <andresfabbiano5@gmail.com>
2022-06-01 23:28:13 -0300

    avance

---
+++ PreEntregas Proyecto/PE1/src/router/api/apiRouter.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const { apiControllers } = require('../../controllers/apiControllers.js')
+
+const productRouter = express.Router();
+
+productRouter.use((req, res, next) => {
+    next();
+})
+
+productRouter.get('/products', apiControllers.products);
+productRouter.get('/products/:id', apiControllers.productById)
+
+productRouter.post('/products', apiControllers.products)
+
+productRouter.put('/products/:id', apiControllers.productById)
+
+productRouter.delete('/products/:id', apiControllers.productById)
+
+const randomProductRouter = express.Router();
+
+randomProductRouter.use((req, res, next) => {
+    next();
+})
+
+randomProductRouter.get('/products', apiControllers.randomProduct);
+
+const messageRouter = express.Router();
+
+messageRouter.post('/messages', apiControllers.messages)
+
+
+module.exports = { productRouter, messageRouter }

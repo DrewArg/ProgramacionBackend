bccb6b98c492267bb909ea91d947cbb91ba6e012
DrewArg <andresfabbiano5@gmail.com>
2022-07-25 11:16:13 -0300

    vista de lista blocking

--- Desafios/D12/client/src/pages/RandomNumbers/RandomNumbers.jsx
+++ Desafios/D12/client/src/pages/RandomNumbers/RandomNumbers.jsx
@@ -2,20 +2,34 @@ import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import '../../style/style.css'
 import { useState } from 'react'
+import './RandomNumbers.css'
 
 function RandomNumbers() {
 
     const [queryNumber, setQueryNumber] = useState(0)
     const [result, setResult] = useState({})
+    const [pKeys, setPkeys] = useState([])
+    const [pValues, setPvalues] = useState([])
+
+    const parseArray = () => {
+        const keys = Object.keys(result)
+        setPkeys(keys)
+        const values = Object.values(result)
+        setPvalues(values)
+
+    }
+
 
     const getBlocking = async () => {
         const url = 'http://localhost:8080/api/blockingRandom'
 
-        console.log(queryNumber);
+        const number = {
+            queryNumber: queryNumber
+        }
 
         await fetch(url, {
             method: "POST",
-            body: queryNumber.toString(),
+            body: JSON.stringify(number),
             headers: {
                 'Access-Control-Allow-Headers': 'Content-Type',
                 'Access-Control-Allow-Credentials': 'true',
@@ -39,17 +53,27 @@ function RandomNumbers() {
     return (
         <>
             <NavBar />
-            <button onClick={getBlocking}>Get blocking</button>
-            <label className="formLabel">Ingresa el número máximo</label>
-            <input className='formInput' type="number" placeholder="número máximo" id="maxNumber" name="maxNumber" value={queryNumber} onInput={e => { setQueryNumber(e.target.value) }} />
-            {result ?
-                <>
-                    {/* {result.map(k=>{
-                       return <div>{k}</div>
-                    })} */}
-                    {console.log(result)}
-                </>
-                : ""}
+            <div className='randomNumbers'>
+
+                <label className="formLabel">Ingresa el número máximo</label>
+                <input className='formInput' type="number" placeholder="número máximo" id="maxNumber" name="maxNumber" value={queryNumber} onInput={e => { setQueryNumber(e.target.value) }} />
+                <button className='buttonOk' onClick={getBlocking}>Get blocking</button>
+                {result ?
+                    <>
+
+                        <button className='buttonOk' onClick={parseArray}>Ver la lista</button>
+                        {
+                            pKeys.map((key, i) => {
+                                return <div key={key}>
+                                    <div >{key}: {pValues[i]}</div>
+                                </div>
+
+
+                            })
+                        }
+                    </>
+                    : ""}
+            </div>
         </>
     )
 }
--- Desafios/D12/server/src/controllers/blockingController.js
+++ Desafios/D12/server/src/controllers/blockingController.js
@@ -1,28 +1,29 @@
 export function calculateBlocking(req, res) {
-  const queryNumber = req.body;
-  const result = {};
+    console.log(req.body);
+    const queryNumber = req.body.queryNumber;
+    const result = {};
 
-  if (queryNumber>0) {
-    for (let i = 0; i < queryNumber; i++) {
-        const rand = Math.floor(Math.random() * queryNumber);
-        if (result[rand]) {
-          result[rand] = result[rand] + 1;
-        } else {
-          result[rand] = 1;
+    if (queryNumber > 0) {
+        for (let i = 0; i < queryNumber; i++) {
+            const rand = Math.floor(Math.random() * queryNumber);
+            if (result[rand]) {
+                result[rand] = result[rand] + 1;
+            } else {
+                result[rand] = 1;
+            }
         }
-      }
-  } else {
-    const millions = 10000000;
-    for (let i = 0; i < millions; i++) {
-      const rand = Math.floor(Math.random() * millions);
-      if (result[rand]) {
-        result[rand] = result[rand] + 1;
-      } else {
-        result[rand] = 1;
-      }
+    } else {
+        const millions = 10000000;
+        for (let i = 0; i < millions; i++) {
+            const rand = Math.floor(Math.random() * millions);
+            if (result[rand]) {
+                result[rand] = result[rand] + 1;
+            } else {
+                result[rand] = 1;
+            }
+        }
     }
-  }
 
-  const string = JSON.stringify(result);
-  res.send(string);
+    const string = JSON.stringify(result);
+    res.send(string);
 }

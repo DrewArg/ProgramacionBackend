50b0522e63ad0a29fb284b5a22f516a4c86becbf
DrewArg <andresfabbiano5@gmail.com>
2022-08-20 12:58:55 -0300

    userControllers js

--- PreEntregas Proyecto/PE3/server/notas.md
+++ PreEntregas Proyecto/PE3/server/notas.md
@@ -1,1 +1,3 @@
 mover las credenciales del config a un dotenv y al example
+ver los //TODO para pensar esa logica faltante
+todos los trycatch que no puedan devolver algo deberian mandarle algo al cliente para que esté informado de que no se pudo hacer X coss -- USAR CODITOS DE ESTADO
---
+++ PreEntregas Proyecto/PE3/server/src/controllers/userControllers.js
@@ -0,0 +1,84 @@
+import bcrypt from 'bcryptjs'
+import { usersDao } from '../daos/daoIndex.js'
+
+export const userController = {
+    async getUsers() {
+        try {
+            const users = await usersDao.listAll()
+            return users
+        } catch (error) {
+            console.log(`User controller --> ${error}`);
+        }
+    },
+
+    async saveUser(user) {
+        try {
+            const doesntExist = await this.isUniqueUsername(user.username)
+            if (doesntExist) {
+                const hashPass = await bcrypt.hash(user.password, 10)
+                const usr = {
+                    username: user.username,
+                    password: hashPass
+                }
+                return await usersDao.saveObject(usr)
+            } else {
+                //TODO devolver que el usuario ya existe, deberia haber una vista para eso
+                console.log(`El usuario ya existe en el sistema`);
+            }
+
+        } catch (error) {
+            console.error(`User controller --> ${error}`);
+        }
+    },
+
+    async getById(id) {
+        try {
+            const usr = await usersDao.listById(id)
+            return usr
+        } catch (error) {
+            console.error(`User controller --> ${error}`);
+        }
+    },
+
+    async getByUsername(username) {
+        try {
+            const users = await this.getUsers()
+            const user = users.find(u => u.username === username)
+            return user
+        } catch (error) {
+            console.error(`User controller --> ${error}`);
+        }
+    },
+
+    async isUniqueUsername(username) {
+        try {
+            const users = await this.getUsers()
+            const user = users.find(u => u.username === username)
+            if (user) {
+                return false
+            } else {
+                return true
+            }
+        } catch (error) {
+            console.error(`User controller --> ${error}`);
+        }
+    },
+
+    async updateUser(user) {
+        try {
+            const usr = await usersDao.updateObject(user)
+            return usr
+        } catch (error) {
+            console.error(`User controller --> ${error}`);
+        }
+    },
+
+    async deleteById(id) {
+        try {
+            await usersDao.deleteById(id)
+            //TODO retornar un mensaje de ok capaz con un codigo de estado
+        } catch (error) {
+            console.error(`User controller --> ${error}`);
+        }
+    }
+}

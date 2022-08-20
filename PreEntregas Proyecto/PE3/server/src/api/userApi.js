92d573dcff0d8a43fdfb937118e8b03a7d933680
DrewArg <andresfabbiano5@gmail.com>
2022-08-20 13:01:48 -0300

    userApi js

---
+++ PreEntregas Proyecto/PE3/server/src/api/userApi.js
@@ -0,0 +1,14 @@
+import { userController } from '../controllers/userControllers.js'
+
+export const registerUser = async (usr) => {
+    try {
+        const user = await userController.saveUser(usr)
+        if (user) {
+            return user
+        } else {
+            console.log(`UserApi --> no se pudo registrar`);
+        }
+    } catch (error) {
+        console.error(`UserApi --> ${error}`);
+    }
+}

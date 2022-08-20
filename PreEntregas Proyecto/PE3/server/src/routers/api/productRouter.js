e4ec0f3c58ed39a079812e44682477f4e5301946
DrewArg <andresfabbiano5@gmail.com>
2022-08-20 16:02:06 -0300

    product router js

---
+++ PreEntregas Proyecto/PE3/server/src/routers/api/productRouter.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import productController from "../../controllers/productControllers";
+
+const productRouter = Router()
+
+productRouter.get('/products', productController.getAllProducts)
+productRouter.post('/products/:id', productController.getById)
+productRouter.post('/products', productController.saveProduct)
+productRouter.put('/products/:id', productController.updateProduct)
+productRouter.delete('/products/:id', productController.deleteById)
+
+export default productRouter

1b85979d1df2a0b12edf65184fcb7a1fc68f33a1
DrewArg <andresfabbiano5@gmail.com>
2022-08-20 16:11:29 -0300

    login controllers js

---
+++ PreEntregas Proyecto/PE3/server/src/controllers/loginControllers.js
@@ -0,0 +1,35 @@
+import { readFile } from "@babel/core/lib/gensync-utils/fs";
+import passport from "passport";
+import { sessionsDao } from "../daos/daoIndex";
+
+export const isLoggedIn = (req, res) => {
+    if (req.session.passport) {
+        return true
+    } else {
+        return false
+    }
+}
+
+export const loginController = (req, res) => {
+    passport.authenticate('local-login', async (error, user, options) => {
+        if (user) {
+            await req.logIn(user, async () => {
+                return await res.json(user)
+            })
+        } else if (options) {
+            return res.json(options)
+        } else {
+            return res.status(204).send('')
+        }
+    })
+}
+
+export const logoutController = (req, res) => {
+    if(isLoggedIn(req,res)){
+        await req.session.destroy()
+        await req.logout()
+        //TODO ver que onda esta linea de abajo que aparece como descomentada siempre
+        // sessionsDao.deleteAll()
+        return res.send('')
+    }
+}

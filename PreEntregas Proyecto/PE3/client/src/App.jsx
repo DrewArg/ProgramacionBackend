6c5ce18b03a96fd94c30c386a653bb8d7c1111ec
DrewArg <andresfabbiano5@gmail.com>
2022-08-21 10:27:28 -0300

    agrega socket context provider a app jsx

--- PreEntregas Proyecto/PE3/client/src/App.jsx
+++ PreEntregas Proyecto/PE3/client/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { SocketContext, socket } from './context/Socket';
 import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <SocketContext.Provider value={socket}>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </SocketContext.Provider>
     </BrowserRouter >
 
 

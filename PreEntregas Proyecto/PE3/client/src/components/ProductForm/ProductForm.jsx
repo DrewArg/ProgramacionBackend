3e17cb9336fabf3a4fe361d4b64807b63cbb89f2
DrewArg <andresfabbiano5@gmail.com>
2022-08-23 12:11:11 -0300

    agrega productos a bd

--- PreEntregas Proyecto/PE3/client/src/components/ProductForm/ProductForm.jsx
+++ PreEntregas Proyecto/PE3/client/src/components/ProductForm/ProductForm.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { useState } from 'react'
 import { SocketContext } from '../../context/Socket.jsx'
+import './ProductForm.css'
 
 const ProductForm = () => {
     const reactSocket = useContext(SocketContext)
@@ -39,20 +40,29 @@ const ProductForm = () => {
             <div className='formContainer'>
                 <h3 className='sectionTitle'>Ingresar Producto</h3>
                 <form className='productForm'>
-                    <label className='formLabel'> Título
+                    <div className='productForm__element'>
+                        <label className='formLabel'> Título </label>
                         <input className='formInput' type="text" placeholder='título del producto' id='prodTitle' name="prodTitle" value={prodTitle} onInput={e => setProdTitle(e.target.value)} />
-                    </label>
-                    <label className='formLabel'> Precio
+                    </div>
+
+                    <div className='productForm__element'>
+                        <label className='formLabel'> Precio </label>
                         <input className='formInput' type="number" placeholder='precio' id='prodPrice' name="prodPrice" value={prodPrice} onInput={e => setProdPrice(e.target.value)} />
-                    </label>
-                    <label className='formLabel'> Imágen
+                    </div>
+
+                    <div className='productForm__element'>
+                        <label className='formLabel'> Imágen </label>
                         <input className='formInput' type="text" placeholder='url de imágen' id='prodThumbnail' name="prodPrice" value={prodThumbnail} onInput={e => setProdThumbnail(e.target.value)} />
-                    </label>
-                    <label className='formLabel'> Descripción
+                    </div>
+
+                    <div className='productForm__element'>
+                        <label className='formLabel'> Descripción </label>
                         <input className='formInput' type="text" placeholder='descripción de la imágen' id='prodAlt' name="prodAlt" value={prodAlt} onInput={e => setProdAlt(e.target.value)} />
-                    </label>
-                    <button className='btn__submit' onClick={addProduct}>Agregar Producto</button>
-                    <button className='btn__submit' onClick={emptyList}>Limpiar formulario</button>
+                    </div>
+                    <div className='btns__div'>
+                        <button className='btn__submit' onClick={addProduct}>Agregar Producto</button>
+                        <button className='btn__submit' onClick={emptyList}>Limpiar formulario</button>
+                    </div>
                 </form>
             </div>
         </>

560aed3e72a0cfdffd3d10a3974ca6fdf74e4029
DrewArg <andresfabbiano5@gmail.com>
2022-08-26 21:21:32 -0300

    prueba 3

--- server/src/server.js
+++ server/src/server.js
@@ -37,7 +37,7 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build', 'index.html'));

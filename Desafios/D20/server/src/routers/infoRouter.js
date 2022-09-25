5f0a88cb0b3c74faf1b71033776c8daf7440256a
DrewArg <andresfabbiano5@gmail.com>
2022-09-25 10:15:42 -0300

    adonis

--- Desafios/D20/server/src/routers/infoRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { infoController } from "../controllers/infoControllers.js";
-
-const infoRouter = Router();
-
-infoRouter.post("/info", (req, res) => infoController(req, res));
-infoRouter.get("/info", (req, res) => infoController(req, res));
-
-export default infoRouter;

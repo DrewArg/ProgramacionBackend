946fcde8eae8f67a84db19ee4b1c5b2b374434fa
DrewArg <andresfabbiano5@gmail.com>
2022-10-03 20:38:35 -0300

    image business logic

---
+++ Proyecto-Final/server/src/models/Image.js
@@ -0,0 +1,54 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export class Image {
+    #id
+    #title
+    #description
+    #url
+
+    /**
+   * @param {string} title
+   * @param {string} description
+   */
+    constructor({ title, description }) {
+        this.#setTitle(title)
+        this.#setDescription(description)
+        this.#id = uuidv4()
+    }
+
+    #setTitle(title) {
+        if (!title) throw new Error(`The title value is required`)
+        if (typeof title !== 'string') throw new Error(`The datatype of title must be a string`)
+        this.#title = title
+    }
+
+    #setDescription(description) {
+        if (!description) throw new Error(`The description value is required`)
+        if (typeof description !== 'string') throw new Error(`The datatype of description must be a string`)
+        this.#description = description
+    }
+
+    /**
+    * @param {string} url
+    */
+    set url(url) {
+        if (!url) throw new Error(`The url value is required`)
+        if (typeof url !== 'string') throw new Error(`The datatype of url must be a string`)
+        this.#url = url
+    }
+
+    get id() { return this.#id }
+    get title() { return this.#title }
+    get description() { return this.#description }
+    get url() { return this.#url }
+
+    getImageData() {
+        return Object.freeze({
+            id: this.#id,
+            title: this.#title,
+            description: this.#description,
+            url: this.#url
+
+        })
+    }
+}

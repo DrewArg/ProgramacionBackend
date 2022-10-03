901f1ee0a59ab2f0e1cf7e60fb4109ad1e793c95
DrewArg <andresfabbiano5@gmail.com>
2022-10-03 20:27:58 -0300

    user business logic}

--- Proyecto-Final/server/src/models/User.js
+++ Proyecto-Final/server/src/models/User.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export class User {
     #id
     #email
@@ -7,9 +9,14 @@ export class User {
     #phone
     #image
 
+    /**
+    * @param {string} email
+    * @param {string} password
+    */
     constructor({ email, password }) {
         this.#setEmail(email)
         this.#setPassword(password)
+        this.#id = uuidv4()
     }
 
     #setEmail(email) {
@@ -19,9 +26,72 @@ export class User {
     }
 
     #setPassword(password) {
-        if (!password) throw new Error(`The password is required`)
+        if (!password) throw new Error(`The password value is required`)
         if (typeof password !== 'string') throw new Error(`The datatype of password must be a string`)
+        this.#password = password
     }
 
-    
+    /**
+     * @param {string} name
+     */
+    set name(name) {
+        if (!name) throw new Error(`The name value is required`)
+        if (typeof name !== 'string') throw new Error(`The datatype of name must be a string`)
+        this.#name = name
+    }
+
+    /**
+     * @param {string} lastname
+     */
+    set lastname(lastname) {
+        if (!lastname) throw new Error(`The lastname value is required`)
+        if (typeof lastname !== 'string') throw new Error(`The datatype of lastname must be a string`)
+        this.#lastname = lastname
+    }
+
+    /**
+     * @param {number} phone
+     */
+    set phone(phone) {
+        if (!phone) throw new Error(`The phone value is required`)
+        if (typeof phone !== 'number') throw new Error(`The datatype of phone must be a number`)
+        this.#phone = phone
+    }
+
+    /**
+     * @param {string} image
+     */
+    set image(image) {
+        if (!image) throw new Error(`The image value is required`)
+        if (typeof image !== 'string') throw new Error(`The datatype of image must be a string`)
+        this.#image = image
+    }
+
+    get id() { return this.#id }
+    get email() { return this.#email }
+    get password() { return this.#password }
+    get name() { return this.#name }
+    get lastname() { return this.#lastname }
+    get phone() { return this.#phone }
+    get image() { return this.#image }
+
+
+    getUserData() {
+        return Object.freeze({
+            id: this.#id,
+            email: this.#email,
+            password: this.#password,
+            name: this.#name,
+            lastname: this.#lastname,
+            phone: this.#phone,
+            image: this.#image
+        })
+    }
+
+
+
+
+
+
+
 }

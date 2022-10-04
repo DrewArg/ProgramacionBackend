0fcb2b2ce9d29070ae1ae90b801871989d63395d
DrewArg <andresfabbiano5@gmail.com>
2022-10-03 22:55:39 -0300

    error handler to server js

--- Proyecto-Final/server/src/server.js
+++ Proyecto-Final/server/src/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { Server as HttpServer } from 'http'
+import { errorHandler } from './middlewares/errorHandler'
 
 const app = express()
 
 app.use('/public', express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(errorHandler)
 
 const httpServer = new HttpServer(app)
 

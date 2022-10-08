9a8393362e41909b8a13e0bbc67ca54c22a7aa2e
drewArg <andresfabbiano5@gmail.com>
2022-10-07 22:48:28 -0300

    make it better

--- PreEntregas Proyecto/PE3/server/src/middlewares/multer.js
+++ PreEntregas Proyecto/PE3/server/src/middlewares/multer.js
@@ -1,19 +1,17 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/images')
-    },
-    filename: function (req, file, cb) {
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.toLowerCase().split(" ").join("-");
 
-        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+    cb(null, `${Date.now()}-${fileName}`);
+  },
+});
 
-        cb(null, `${Date.now()}-${fileName}`)
-    }
-})
+const upload = multer({ storage });
 
-const upload = multer({ storage })
-
-export const oneFileMiddleware = upload.single('profileImg')
-export const multipleFilesMiddleware = upload.array('myFiles')
-
+export const oneFileMiddleware = upload.single("profileImg");
+export const multipleFilesMiddleware = upload.array("myFiles");
